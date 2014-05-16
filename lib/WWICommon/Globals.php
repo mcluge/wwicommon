b662c81fdf736a267422e7df52f567b5a17b75dd
Michael Pearson <mpearson@westwaveproductions.com>
2014-05-15 22:20:14 -0600

    set

--- lib/WWICommon/Globals.php
+++ lib/WWICommon/Globals.php
@@ -9,12 +9,21 @@ class Globals{
 
 	public static function get($prop)
 	{
-
+		if(isset(self::$prop)) {
+			return self::$prop;
+		}else{
+			trigger_error("Developer error:: property does not exist!");
+		}
 	}
 
 	public static function set($prop,$value)
 	{
-		
+		if(self::$prop = $value) {
+			return true;
+		}else{
+			trigger_error("Developer error:: property does not exist!");
+		}
+
 	}
 
 }
