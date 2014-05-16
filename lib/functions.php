fa19a809de7c232adf31d4189cdc609e3f251788
Michael Pearson <mpearson@westwaveproductions.com>
2014-05-15 20:53:57 -0600

    add functions and license

---
+++ lib/functions.php
@@ -0,0 +1,25 @@
+<?php
+
+	if( !function_exists( "dbug" ) ) {
+		function dbug( $data) {
+			$sapi_type = php_sapi_name();
+			$tr = debug_backtrace();
+			$level = 0;
+			$color = null;
+			$colorReset = null;
+			if($sapi_type != "cli"){
+				$color = "\033";
+				$colorReset = "\033[0m";
+			}
+			if($sapi_type == "cli"){
+				echo "\033[35m"."DBUG called line ".$tr[$level]['line']." of ".$tr[$level]['file']."\n"."\033[31m";
+				echo "\033[31m".var_export( func_get_args() )."\n"."\033[0m";
+
+			}else{
+				echo "<pre>";
+				var_export( func_get_args() )."\n";
+				echo "</pre>";
+			}
+
+		}
+	}
